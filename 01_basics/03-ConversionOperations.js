4b5ae162e26dc55ace9dc3ff5f6eb7161dc455cb
Tanishka Borade <117269757+Tanishka33@users.noreply.github.com>
2025-01-28 14:33:08 +0000

    Conversion Operations

---
+++ 01_basics/03-ConversionOperations.js
@@ -0,0 +1,34 @@
+// let score = "aa33c"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber);
+
+// let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33"- 33
+// "33abc" - NaN
+// true -  1; false - 0
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 - true; 0 - false
+// "" - false
+// "tanu" - true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
