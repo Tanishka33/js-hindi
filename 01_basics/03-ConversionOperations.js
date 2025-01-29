c4f3d1d81787d4f1440439ebbba4dff8475c9b78
Tanishka Borade <117269757+Tanishka33@users.noreply.github.com>
2025-01-29 14:22:26 +0000

    Comparision and datatypes

--- 01_basics/03-ConversionOperations.js
+++ 01_basics/03-ConversionOperations.js
@@ -28,7 +28,34 @@
 // "" - false
 // "tanu" - true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// ******************OPERTATIONS************
+
+// let value =  3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+
+// let str1 = 'hello'
+// let str2 = ' tanu'
+// let str3 = str1 + str2
+// console.log(str3); 
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
