0ece503b942259b3c69107e6889985548eda53c4
Tanishka Borade <117269757+Tanishka33@users.noreply.github.com>
2025-02-22 07:24:45 +0000

    understood the concept of stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,17 +1,17 @@
 // Primitive
 
 // 7 types - String, Number, Boolean, null, undefined, Symbol, BigInt
-const score = 100
-const scoreValue = 100.3
-const isLoggedIn = false
-const outsideTemp = null
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
 
-let userEmail; //undefined
+// let userEmail; //undefined
 
-const id = Symbol('123')
-const anotherID = Symbol('123')
+// const id = Symbol('123')
+// const anotherID = Symbol('123')
 
-console.log(id === anotherID);
+// console.log(id === anotherID);
 
 // const bigNumber = 3445677899277n
 
@@ -20,14 +20,36 @@ console.log(id === anotherID);
 // Reference (Non Primitive)
 // Array, Objects, Functions
 
-const heros = ["saktiman", "naagraj", "doga"]
-let myObj = {
-    name: "Tanu",
-    age: 20,
-}
+// const heros = ["saktiman", "naagraj", "doga"]
+// let myObj = {
+//     name: "Tanu",
+//     age: 20,
+// }
 
-const myFunction = function() {
-    console.log("Hello world");
-}
+// const myFunction = function() {
+//     console.log("Hello world");
+// }
 
-console.log(typeof bigNumber) // determines the datatype of 
+// console.log(typeof bigNumber) // determines the datatype of 
+
+
+
+// ++++++++++++++++++++
+// Stack (Primitive), Heap(Nop-primitive)
+
+let myYoutubename = "tanura"
+let anothername = myYoutubename
+anothername = "yayyeresa"
+console.log(anothername)
+console.log(myYoutubename)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+
+userTwo.email = "tanu.google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
